ac4e8943db3275ce8e6eef38c4f5a81be052559b
Dplex <dplex@outlook.com>
2019-07-30 21:46:31 +0900

    Adjust base64 encoding scret text to save yaml file

--- src/main/kotlin/com/kakaopay/assignment/util/CipherUtil.kt
+++ src/main/kotlin/com/kakaopay/assignment/util/CipherUtil.kt
@@ -9,6 +9,7 @@ import javax.crypto.Cipher
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import org.apache.coyote.http11.Constants.a
+import java.util.*
 import kotlin.experimental.and
 
 
@@ -21,21 +22,25 @@ class CipherUtil {
     lateinit var cipher: Cipher
     lateinit var keySpec: SecretKeySpec
     lateinit var gcmSpec: GCMParameterSpec
-
+    lateinit var encoder: Base64.Encoder
+    lateinit var decoder: Base64.Decoder
     init {
         cipher = Cipher.getInstance("AES/GCM/NoPadding")
         keySpec = SecretKeySpec(key!!.toByteArray(), "AES")
         gcmSpec = GCMParameterSpec(128, key!!.toByteArray())
+        encoder = Base64.getEncoder()
+        decoder = Base64.getDecoder()
     }
 
     fun encryptCbc(text: ByteArray): ByteArray {
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmSpec)
-        return cipher.doFinal(text)
+        return encoder.encode(cipher.doFinal(text))
     }
 
     fun decrypteCbc(text: String): String {
+
         cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmSpec)
-        return cipher.doFinal(text.toByteArray(Charset.defaultCharset())).toString(Charset.defaultCharset())
+        return cipher.doFinal(decoder.decode(text.toByteArray(Charset.defaultCharset()))).toString(Charset.defaultCharset())
     }
 }
 
@@ -44,6 +49,6 @@ fun main() {
 
     val pw = cipherUtil.encryptCbc("!q2w3e4r".toByteArray(Charset.defaultCharset()))
     println(pw.toString(Charset.defaultCharset()))
-//    val plain = cipherUtil.decrypteCbc(pw)
-//    println(plain.toString(Charset.defaultCharset()))
+    val plain = cipherUtil.decrypteCbc(pw.toString(Charset.defaultCharset()))
+    println(plain)
 }
